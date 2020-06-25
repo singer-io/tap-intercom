aa5223e20966cee24dc0c3b82bca0e2d20cc9e21
Kyle Allan <kyle@stitchdata.com>
2020-06-25 15:03:22 +0000

    bump to 0.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-intercom',
-      version='0.0.3',
+      version='0.0.4',
       description='Singer.io tap for extracting data from the Intercom API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
