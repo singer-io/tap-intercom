78ebc870da70535ca11a333fc449f0dde96a233e
Asaf Erlich <aerlich@talend.com>
2020-09-30 14:02:29 -0400

    Bumping to first major version 1.0.0 for GA Release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-intercom',
-      version='0.1.0',
+      version='1.0.0',
       description='Singer.io tap for extracting data from the Intercom API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
