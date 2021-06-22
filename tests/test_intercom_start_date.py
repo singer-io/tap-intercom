58f67c1aaa17016ebb86955caebea829d33e4cd5
Andrey Kabanov <andrey.kabanov@bytecode.io>
2021-06-21 17:02:01 -0700

    * remove get_properties as it's defined in base; * remove irrelevant check for is_insight()

--- tests/test_intercom_start_date.py
+++ tests/test_intercom_start_date.py
@@ -1,5 +1,3 @@
-import os
-
 from tap_tester import connections, runner
 
 from base import IntercomBaseTest
@@ -13,19 +11,6 @@ class IntercomStartDateTest(IntercomBaseTest):
     @staticmethod
     def name():
         return "tap_tester_intercom_start_date_test"
-
-    def get_properties(self, original: bool = True):
-        """Configuration properties required for the tap."""
-        return_value = {
-            'access_token': os.getenv('TAP_INTERCOM_ACCESS_TOKEN'),
-            'start_date' : '2019-07-22T00:00:00Z',
-            'user_agent': 'tap-intercom <api_user_email@your_company.com>',
-        }
-        if original:
-            return return_value
-
-        return_value["start_date"] = self.start_date
-        return return_value
 
     def test_run(self):
         """Instantiate start date according to the desired data set and run the test"""
@@ -87,9 +72,6 @@ class IntercomStartDateTest(IntercomBaseTest):
 
                 # expected values
                 expected_primary_keys = self.expected_primary_keys()[stream]
-                expected_insights_buffer = -1 * int(self.get_properties()['insights_buffer_days'])
-                expected_start_date_1 = self.timedelta_formatted(self.start_date_1, days=expected_insights_buffer)
-                expected_start_date_2 = self.timedelta_formatted(self.start_date_2, days=expected_insights_buffer)
 
                 # collect information for assertions from syncs 1 & 2 base on expected values
                 record_count_sync_1 = record_count_by_stream_1.get(stream, 0)
@@ -103,47 +85,9 @@ class IntercomStartDateTest(IntercomBaseTest):
                 primary_keys_sync_1 = set(primary_keys_list_1)
                 primary_keys_sync_2 = set(primary_keys_list_2)
 
-                if self.is_insight(stream):
+                # Verify that the 2nd sync with a later start date replicates the same number of
+                # records as the 1st sync.
+                self.assertEqual(record_count_sync_2, record_count_sync_1)
 
-                    # collect information specific to incremental streams from syncs 1 & 2
-                    expected_replication_key = next(iter(self.expected_replication_keys().get(stream)))
-                    replication_dates_1 =[row.get('data').get(expected_replication_key) for row in
-                                          synced_records_1.get(stream, {'messages': []}).get('messages', [])
-                                          if row.get('data')]
-                    replication_dates_2 =[row.get('data').get(expected_replication_key) for row in
-                                          synced_records_2.get(stream, {'messages': []}).get('messages', [])
-                                          if row.get('data')]
-
-                    # # Verify replication key is greater or equal to start_date for sync 1
-                    for replication_date in replication_dates_1:
-                        self.assertGreaterEqual(
-                            self.parse_date(replication_date), self.parse_date(expected_start_date_1),
-                                msg="Report pertains to a date prior to our start date.\n" +
-                                "Sync start_date: {}\n".format(expected_start_date_1) +
-                                "Record date: {} ".format(replication_date)
-                        )
-
-                    # Verify replication key is greater or equal to start_date for sync 2
-                    for replication_date in replication_dates_2:
-                        self.assertGreaterEqual(
-                            self.parse_date(replication_date), self.parse_date(expected_start_date_2),
-                                msg="Report pertains to a date prior to our start date.\n" +
-                                "Sync start_date: {}\n".format(expected_start_date_2) +
-                                "Record date: {} ".format(replication_date)
-                        )
-
-                    # Verify the number of records replicated in sync 1 is greater than the number
-                    # of records replicated in sync 2
-                    self.assertGreater(record_count_sync_1, record_count_sync_2)
-
-                    # Verify the records replicated in sync 2 were also replicated in sync 1
-                    self.assertTrue(primary_keys_sync_2.issubset(primary_keys_sync_1))
-
-                else:
-
-                    # Verify that the 2nd sync with a later start date replicates the same number of
-                    # records as the 1st sync.
-                    self.assertEqual(record_count_sync_2, record_count_sync_1)
-
-                    # Verify by primary key the same records are replicated in the 1st and 2nd syncs
-                    self.assertSetEqual(primary_keys_sync_1, primary_keys_sync_2)
+                # Verify by primary key the same records are replicated in the 1st and 2nd syncs
+                self.assertSetEqual(primary_keys_sync_1, primary_keys_sync_2)
