159c3988a1972f8d949efd542a30fc03c7996df6
prijendev <prijen.khokhani@crestdatasys.com>
2021-12-14 19:12:15 +0530

    Updated unit test cases

--- tests/unittests/test_request_timeouts.py
+++ tests/unittests/test_request_timeouts.py
@@ -10,13 +10,11 @@ REQUEST_TIMEOUT_FLOAT = 300.0
 
 @patch("time.sleep")
 @patch("requests.Session.get", side_effect=Timeout)
-class TestRequestTimeoutsInCheckAccessToken(unittest.TestCase):
-    """
-    Verify that tap retry 5 times on Timeout error raise from check_access_token method
-    """
-    def test_no_request_timeout_in_config(self, mocked_get, mock_sleep):
+class TestRequestTimeoutsBackoff(unittest.TestCase):
+
+    def test_request_timeout_backoff_in_check_access_token(self, mocked_request, mocked_sleep):
         """
-        Verify that if request_timeout is not provided in config then default value is used
+            Verify check_access_token function is backoff for 5 times on Timeout exceeption
         """
         client = IntercomClient('dummy_token', None)
         try:
@@ -24,121 +22,26 @@ class TestRequestTimeoutsInCheckAccessToken(unittest.TestCase):
         except Timeout:
             pass
 
-        # Verify requests.Session.get is called with expected timeout
-        args, kwargs = mocked_get.call_args
-        self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_INT) # Verify timeout argument
-
         # Verify that requests.Session.request is called 5 times
-        self.assertEqual(mocked_get.call_count, 5)
+        self.assertEqual(mocked_request.call_count, 5)
 
-    def test_integer_request_timeout_in_config(self, mocked_get, mock_sleep):
+    def test_request_timeout_backoff_in_request(self, mocked_request, mocked_sleep):
         """
-            Verify that if request_timeout is provided in config(integer value) then it should be use
-        """
-        client = IntercomClient('dummy_token', REQUEST_TIMEOUT_INT)# int timeout in config
-        try:
-            client.__enter__()
-        except Timeout:
-            pass
-
-        # Verify requests.Session.get is called with expected timeout
-        args, kwargs = mocked_get.call_args
-        self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_FLOAT) # Verify timeout argument
-
-        # Verify that requests.Session.get is called 5 times
-        self.assertEqual(mocked_get.call_count, 5)
-
-    def test_float_request_timeout_in_config(self, mocked_get, mock_sleep):
-        """
-            Verify that if request_timeout is provided in config(float value) then it should be use
-        """
-        client = IntercomClient('dummy_token', REQUEST_TIMEOUT_FLOAT)# float timeout in config
-        try:
-            client.__enter__()
-        except Timeout:
-            pass
-        # Verify requests.Session.get is called with expected timeout
-        args, kwargs = mocked_get.call_args
-        self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_FLOAT) # Verify timeout argument
-
-        # Verify that requests.Session.get is called 5 times
-        self.assertEqual(mocked_get.call_count, 5)
-
-    def test_string_request_timeout_in_config(self, mocked_get, mock_sleep):
-        """
-            Verify that if request_timeout is provided in config(string value) then it should be use
-        """
-        client = IntercomClient('dummy_token', REQUEST_TIMEOUT_INT)# string timeout in config
-        try:
-            client.__enter__()
-        except Timeout:
-            pass
-
-        # Verify requests.Session.get is called with expected timeout
-        args, kwargs = mocked_get.call_args
-        self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_FLOAT) # Verify timeout argument
-
-        # Verify that requests.Session.get is called 5 times
-        self.assertEqual(mocked_get.call_count, 5)
-
-    def test_empty_string_request_timeout_in_config(self, mocked_get, mock_sleep):
-        """
-            Verify that if request_timeout is provided in config with empty string then default value is used
-        """
-        client = IntercomClient('dummy_token', "")# empty string timeout in config
-        try:
-            client.__enter__()
-        except Timeout:
-            pass
-
-        # Verify requests.Session.get is called with expected timeout
-        args, kwargs = mocked_get.call_args
-        self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_INT) # Verify timeout argument
-
-        # Verify that requests.Session.get is called 5 times
-        self.assertEqual(mocked_get.call_count, 5)
-
-    def test_zero_request_timeout_in_config(self, mocked_get, mock_sleep):
-        """
-            Verify that if request_timeout is provided in config with zero value then default value is used
-        """
-        client = IntercomClient('dummy_token', 0)# int zero value timeout in config
-        try:
-            client.__enter__()
-        except Timeout:
-            pass
-
-        # Verify requests.Session.get is called with expected timeout
-        args, kwargs = mocked_get.call_args
-        self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_INT) # Verify timeout argument
-
-        # Verify that requests.Session.get is called 5 times
-        self.assertEqual(mocked_get.call_count, 5)
-
-    def test_zero_string_request_timeout_in_config(self, mocked_get, mock_sleep):
-        """
-            Verify that if request_timeout is provided in config with zero in string format then default value is used
+            Verify request function is backoff for 5 times on Timeout exceeption
         """
         client = IntercomClient('dummy_token', REQUEST_TIMEOUT_INT)# string 0 timeout in config
         try:
-            client.__enter__()
+            client.request("base_url")
         except Timeout:
             pass
 
-        # Verify requests.Session.get is called with expected timeout
-        args, kwargs = mocked_get.call_args
-        self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_INT) # Verify timeout argument
-
-        # Verify that requests.Session.get is called 5 times
-        self.assertEqual(mocked_get.call_count, 5)
+        # Verify that requests.Session.request is called 5 times
+        self.assertEqual(mocked_request.call_count, 5)
 
 @patch("time.sleep")
 @patch("requests.Session.request", side_effect=Timeout)
 @patch("tap_intercom.client.IntercomClient.check_access_token")
-class TestRequestTimeoutsInRequestMethod(unittest.TestCase):
-    """
-    Verify that tap retry 5 times on Timeout error raise from request method
-    """
+class TestRequestTimeoutsValue(unittest.TestCase):
     def test_no_request_timeout_in_config(self, mocked_token, mocked_request, mock_sleep):
         """
         Verify that if request_timeout is not provided in config then default value is used
@@ -153,9 +56,6 @@ class TestRequestTimeoutsInRequestMethod(unittest.TestCase):
         args, kwargs = mocked_request.call_args
         self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_INT) # Verify timeout argument
 
-        # Verify that requests.Session.request is called 5 times
-        self.assertEqual(mocked_request.call_count, 5)
-
     def test_integer_request_timeout_in_config(self, mocked_token, mocked_request, mock_sleep):
         """
             Verify that if request_timeout is provided in config(integer value) then it should be use
@@ -166,13 +66,9 @@ class TestRequestTimeoutsInRequestMethod(unittest.TestCase):
         except Timeout:
             pass
 
-
         # Verify requests.Session.request is called with expected timeout
         args, kwargs = mocked_request.call_args
         self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_FLOAT) # Verify timeout argument
-
-        # Verify that requests.Session.request is called 5 times
-        self.assertEqual(mocked_request.call_count, 5)
 
     def test_float_request_timeout_in_config(self, mocked_token, mocked_request, mock_sleep):
         """
@@ -186,9 +82,6 @@ class TestRequestTimeoutsInRequestMethod(unittest.TestCase):
         # Verify requests.Session.request is called with expected timeout
         args, kwargs = mocked_request.call_args
         self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_FLOAT) # Verify timeout argument
-
-        # Verify that requests.Session.request is called 5 times
-        self.assertEqual(mocked_request.call_count, 5)
 
     def test_string_request_timeout_in_config(self, mocked_token, mocked_request, mock_sleep):
         """
@@ -204,9 +97,6 @@ class TestRequestTimeoutsInRequestMethod(unittest.TestCase):
         args, kwargs = mocked_request.call_args
         self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_FLOAT) # Verify timeout argument
 
-        # Verify that requests.Session.request is called 5 times
-        self.assertEqual(mocked_request.call_count, 5)
-
     def test_empty_string_request_timeout_in_config(self, mocked_token, mocked_request, mock_sleep):
         """
             Verify that if request_timeout is provided in config with empty string then default value is used
@@ -220,9 +110,6 @@ class TestRequestTimeoutsInRequestMethod(unittest.TestCase):
         # Verify requests.Session.request is called with expected timeout
         args, kwargs = mocked_request.call_args
         self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_INT) # Verify timeout argument
-
-        # Verify that requests.Session.request is called 5 times
-        self.assertEqual(mocked_request.call_count, 5)
 
     def test_zero_request_timeout_in_config(self, mocked_token, mocked_request, mock_sleep):
         """
@@ -238,9 +125,6 @@ class TestRequestTimeoutsInRequestMethod(unittest.TestCase):
         args, kwargs = mocked_request.call_args
         self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_INT) # Verify timeout argument
 
-        # Verify that requests.Session.request is called 5 times
-        self.assertEqual(mocked_request.call_count, 5)
-
     def test_zero_string_request_timeout_in_config(self, mocked_token, mocked_request, mock_sleep):
         """
             Verify that if request_timeout is provided in config with zero in string format then default value is used
@@ -254,6 +138,3 @@ class TestRequestTimeoutsInRequestMethod(unittest.TestCase):
         # Verify requests.Session.request is called with expected timeout
         args, kwargs = mocked_request.call_args
         self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_INT) # Verify timeout argument
-
-        # Verify that requests.Session.request is called 5 times
-        self.assertEqual(mocked_request.call_count, 5)
