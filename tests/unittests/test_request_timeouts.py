91457eecc536916d868fd720246719344fa7efea
prijendev <prijen.khokhani@crestdatasys.com>
2021-12-15 09:56:11 +0530

    Removed unused mock sleep

--- tests/unittests/test_request_timeouts.py
+++ tests/unittests/test_request_timeouts.py
@@ -48,11 +48,10 @@ class TestRequestTimeoutsBackoff(unittest.TestCase):
         # Verify that requests.Session.request is called 5 times
         self.assertEqual(mocked_request.call_count, 5)
 
-@patch("time.sleep")
 @patch("requests.Session.request", side_effect = get_mock_http_response)
 @patch("tap_intercom.client.IntercomClient.check_access_token")
 class TestRequestTimeoutsValue(unittest.TestCase):
-    def test_no_request_timeout_in_config(self, mocked_token, mocked_request, mock_sleep):
+    def test_no_request_timeout_in_config(self, mocked_token, mocked_request):
         """
         Verify that if request_timeout is not provided in config then default value is used
         """
@@ -66,7 +65,7 @@ class TestRequestTimeoutsValue(unittest.TestCase):
         args, kwargs = mocked_request.call_args
         self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_INT) # Verify timeout argument
 
-    def test_integer_request_timeout_in_config(self, mocked_token, mocked_request, mock_sleep):
+    def test_integer_request_timeout_in_config(self, mocked_token, mocked_request):
         """
             Verify that if request_timeout is provided in config(integer value) then it should be use
         """
@@ -80,7 +79,7 @@ class TestRequestTimeoutsValue(unittest.TestCase):
         args, kwargs = mocked_request.call_args
         self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_FLOAT) # Verify timeout argument
 
-    def test_float_request_timeout_in_config(self, mocked_token, mocked_request, mock_sleep):
+    def test_float_request_timeout_in_config(self, mocked_token, mocked_request):
         """
             Verify that if request_timeout is provided in config(float value) then it should be use
         """
@@ -94,7 +93,7 @@ class TestRequestTimeoutsValue(unittest.TestCase):
         args, kwargs = mocked_request.call_args
         self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_FLOAT) # Verify timeout argument
 
-    def test_string_request_timeout_in_config(self, mocked_token, mocked_request, mock_sleep):
+    def test_string_request_timeout_in_config(self, mocked_token, mocked_request):
         """
             Verify that if request_timeout is provided in config(string value) then it should be use
         """
@@ -108,7 +107,7 @@ class TestRequestTimeoutsValue(unittest.TestCase):
         args, kwargs = mocked_request.call_args
         self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_FLOAT) # Verify timeout argument
 
-    def test_empty_string_request_timeout_in_config(self, mocked_token, mocked_request, mock_sleep):
+    def test_empty_string_request_timeout_in_config(self, mocked_token, mocked_request):
         """
             Verify that if request_timeout is provided in config with empty string then default value is used
         """
@@ -122,13 +121,13 @@ class TestRequestTimeoutsValue(unittest.TestCase):
         args, kwargs = mocked_request.call_args
         self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_INT) # Verify timeout argument
 
-    def test_zero_request_timeout_in_config(self, mocked_token, mocked_request, mock_sleep):
+    def test_zero_request_timeout_in_config(self, mocked_token, mocked_request):
         """
             Verify that if request_timeout is provided in config with zero value then default value is used
         """
         # Initialize IntercomClient object with request_timeout
         client = IntercomClient('dummy_token', 0)# int zero value timeout in config
-   
+        
         # Call request method which call requests.Session.request with timeout
         client.request("base_url")
 
@@ -136,13 +135,13 @@ class TestRequestTimeoutsValue(unittest.TestCase):
         args, kwargs = mocked_request.call_args
         self.assertEqual(kwargs.get('timeout'), REQUEST_TIMEOUT_INT) # Verify timeout argument
 
-    def test_zero_string_request_timeout_in_config(self, mocked_token, mocked_request, mock_sleep):
+    def test_zero_string_request_timeout_in_config(self, mocked_token, mocked_request):
         """
             Verify that if request_timeout is provided in config with zero in string format then default value is used
         """
         # Initialize IntercomClient object with request_timeout
         client = IntercomClient('dummy_token', "0")# string 0 timeout in config
-     
+        
         # Call request method which call requests.Session.request with timeout
         client.request("base_url")
 
