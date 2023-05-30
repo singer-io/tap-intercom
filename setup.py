8d10a4d43bd9efde381c975ca7da3248515e0b06
Andy Lu <andy@stitchdata.com>
2023-05-30 20:26:25 +0000

    Bump version, update changelog

--- setup.py
+++ setup.py
@@ -1,1 +1,33 @@
-clojure.lang.LazySeq@2fe7c941
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='tap-intercom',
+      version='2.0.1',
+      description='Singer.io tap for extracting data from the Intercom API',
+      author='jeff.huth@bytecode.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_intercom'],
+      install_requires=[
+          'backoff==1.8.0',
+          'requests==2.31.0',
+          'singer-python==5.10.0'
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-intercom=tap_intercom:main
+      ''',
+      packages=find_packages(),
+      package_data={
+          'schemas': ['tap_intercom/schemas/*.json']
+       },
+       include_package_data=True,
+      extras_require={
+          'dev': [
+              'pylint==2.14.5',
+              'ipdb',
+              'nose',
+              'parameterized'
+          ]
+      }
+      )
