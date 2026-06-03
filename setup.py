1c5e4cfbcbcfb6b481859047e5a9f38e24419b89
Leslie VanDeMark <38043390+leslievandemark@users.noreply.github.com>
2026-06-03 10:03:39 -0400

    Bump requests to 2.33.0 and version to 2.2.5 (#90)

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-intercom',
-      version='2.2.4',
+      version='2.2.5',
       description='Singer.io tap for extracting data from the Intercom API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_intercom'],
       install_requires=[
           'backoff==2.2.1',
-          'requests==2.32.4',
+          'requests==2.33.0',
           'singer-python==6.0.1'
       ],
       entry_points='''
