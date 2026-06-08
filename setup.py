4ac1a611bc02669f2501a015e4786aee1cd9dfd3
Rushikesh Todkar <98420315+RushiT0122@users.noreply.github.com>
2026-06-08 17:49:28 +0530

    Update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(name='tap-intercom',
       py_modules=['tap_intercom'],
       install_requires=[
           'backoff==2.2.1',
-          'requests==2.32.4',
-          'singer-python==6.0.1'
+          'requests==2.34.2',
+          'singer-python==6.8.0'
       ],
       entry_points='''
           [console_scripts]
